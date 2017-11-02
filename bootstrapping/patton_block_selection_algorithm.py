da01e08378efd2f93658484a94016f2856bcdd02
Travis Howe <b.travis.howe@gmail.com>
2017-11-02 14:56:50 -0500

    tested and cleaned up a bit

--- bootstrapping/patton_block_selection_algorithm.py
+++ bootstrapping/patton_block_selection_algorithm.py
@@ -12,106 +12,8 @@ pd.set_option('display.max_rows', 30000)
 pd.set_option('max_colwidth', 4000)
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
-'''
-
-
-
-
-
-% these are optional in opt_block_length_full.m, but fixed at default values here
-KN=max(5,sqrt(log10(n)));
-%mmax = ceil(sqrt(n));
-mmax = ceil(sqrt(n))+KN;           % adding KN extra lags to employ Politis' (2002) suggestion for finding largest signif m
-warning_flags=0;
-round=0;
-%Bmax = sqrt(n);                  % maximum value of Bstar to consider.
-Bmax = ceil(min(3*sqrt(n),n/3));  % dec07: new idea for rule-of-thumb to put upper bound on estimated optimal block length
-
-c=2;
-origdata=data;
-Bstar_final=[];
-
-for i=1:k
-   data=origdata(:,i);
-
-   % FIRST STEP: finding mhat-> the largest lag for which the autocorrelation is still significant.
-   temp = mlag(data,mmax);
-   temp = temp(mmax+1:end,:);	% dropping the first mmax rows, as they're filled with zeros
-   temp = corrcoef([data(mmax+1:end),temp]);
-   temp = temp(2:end,1);
-
-   % We follow the empirical rule suggested in Politis, 2002, "Adaptive Bandwidth Choice".
-   % as suggested in Remark 2.3, setting c=2, KN=5
-   temp2 = [mlag(temp,KN)',temp(end-KN+1:end)];		% looking at vectors of autocorrels, from lag mhat to lag mhat+KN
-   temp2 = temp2(:,KN+1:end);		% dropping the first KN-1, as the vectors have empty cells
-   temp2 = (abs(temp2)<(c*sqrt(log10(n)/n)*ones(KN,mmax-KN+1)));	% checking which are less than the critical value
-   temp2 = sum(temp2)';		% this counts the number of insignif autocorrels
-   temp3 = [(1:1:length(temp2))',temp2];
-   temp3 = temp3(find(temp2==KN),:);	% selecting all rows where ALL KN autocorrels are not signif
-   if isempty(temp3)
-      mhat = max(find(abs(temp) > (c*sqrt(log10(n)/n)) )); % this means that NO collection of KN autocorrels were all insignif, so pick largest significant lag
-   else
-      mhat = temp3(1,1);	% if more than one collection is possible, choose the smallest m
-   end
-   if 2*mhat>mmax;
-      M = mmax;
-      trunc1=1;
-   else
-      M = 2*mhat;
-   end
-   clear temp temp2 temp3;
-
-
-   % SECOND STEP: computing the inputs to the function for Bstar
-   kk = (-M:1:M)';
-
-   if M>0;
-      temp = mlag(data,M);
-      temp = temp(M+1:end,:);	% dropping the first mmax rows, as they're filled with zeros
-      temp = cov([data(M+1:end),temp]);
-      acv = temp(:,1);			% autocovariances
-      acv2 = [-(1:1:M)',acv(2:end)];
-      if size(acv2,1)>1;
-         acv2 = sortrows(acv2,1);
-      end
-      acv = [acv2(:,2);acv];			% autocovariances from -M to M
-      clear acv2;
-      Ghat = sum(lam(kk/M).*abs(kk).*acv);
-      DCBhat = 4/3*sum(lam(kk/M).*acv)^2;
-
-% OLD nov07
-%      DSBhat = 2/pi*quadl('opt_block_length_calc',-pi,pi,[],[],kk,acv,lam(kk/M));
-%      DSBhat = DSBhat + 4*sum(lam(kk/M).*acv)^2;	% first part of DSBhat (note cos(0)=1)
-
-% NEW dec07
-      DSBhat = 2*(sum(lam(kk/M).*acv)^2);	% first part of DSBhat (note cos(0)=1)
-
-      % FINAL STEP: constructing the optimal block length estimator
-      Bstar = ((2*(Ghat^2)/DSBhat)^(1/3))*(n^(1/3));
-      if Bstar>Bmax
-         Bstar = Bmax;
-      end
-      BstarCB = ((2*(Ghat^2)/DCBhat)^(1/3))*(n^(1/3));
-
-      if BstarCB>Bmax
-         BstarCB = Bmax;
-      end
-      Bstar = [Bstar;BstarCB];
-   else
-      Ghat = 0;
-      % FINAL STEP: constructing the optimal block length estimator
-      Bstar = [1;1];
-   end
-   Bstar_final=[Bstar_final Bstar];
-end
-Bstar=Bstar_final;
-
-%%%%%%%%%%%%%%%%%%%%%%%%
-function lam=lam(kk)
-%Helper function, calculates the flattop kernel weights
-lam = (abs(kk)>=0).*(abs(kk)<0.5)+2*(1-abs(kk)).*(abs(kk)>=0.5).*(abs(kk)<=1);
-'''
-
+def lam(x):
+    return np.where((np.abs(x) >= 0), 1, 0) * np.where((np.abs(x) < 0.5), 1, 0) + 2 * (1 - np.abs(x)) * np.where((np.abs(x) >= 0.5), 1, 0) * np.where((np.abs(x) <= 1), 1, 0)
 
 def mlags(series, lags):
     # todo: make the column names indicative of the lag number and the original column name as well
@@ -119,65 +21,84 @@ def mlags(series, lags):
     df.columns = ['L{}'.format(i) for i in xrange(lags + 1)]
     return df
 
-
-def opt_block_length(data):
+def opt_block_length(df, bootstrap_type='Circular', rnd=False):
     """This is a function taken from Andrew Patton (http://public.econ.duke.edu/~ap172/) to select the optimal (in the
     sense of minimising the MSE of the estimator of the long-run variance) block length for the stationary bootstrap or
     circular bootstrap. Code follows Politis and White, 2001, 'Automatic Block-Length Selection for the Dependent
     Bootstrap.'
 
-    INPUTS:	data, an nxk pandas dataframe
+    INPUTS:
+    -data, an nxk pandas dataframe
+    -bootstrap_type, a string indicating whether the optimal bootstrap block lengths should be found for the stationary
+    bootstrap or the circular bootstrap
+    -rnd, whether the estimtes should be rounded or not
 
     OUTPUTS: Bstar, a 2xk vector of optimal bootstrap block lengths, [BstarSB;BstarCB]
 
 """
-    n, k = data.shape
-    print n, k
-    #
+    n, k = df.shape
+
     KN = int(np.maximum(5, np.sqrt(np.log10(n))))
-    # mmax = int(np.ceil(np.sqrt(n)) + KN)
-    # warning_flags = 0  # todo: ?
-    # round = 0
-    # Bmax = np.ceil(np.minimum(3 * np.sqrt(n), n / 3))
-    #
+    mmax = int(np.ceil(np.sqrt(n)) + KN)
+    Bmax = np.ceil(np.minimum(3 * np.sqrt(n), n / 3))
     c = norm.ppf(0.975)
-    origdata = data
-    # Bstar_final = []
+
+    Bstar = []
 
     for i in xrange(0, k):
-
-        mmax = 2  # todo: delete
-
-        data = origdata.iloc[:, i]
+        data = df.iloc[:, i]
         rho_k = acf(data, nlags=mmax)[1:]  # see note at this point in the code in http://public.econ.duke.edu/~ap172/ regarding sample correlations versus the acf as used here
         rho_k_crit = c * np.sqrt(np.log10(n) / n)
 
         ni_function = lambda x: np.sum((np.abs(rho_k) < rho_k_crit)[x: x + KN])
         num_insignificant = [ni_function(i) for i in xrange(mmax - KN + 1)]
 
-        if any(num_insignificant == KN):
-            mhat = num_insignificant.remove(KN)
-            # mhat = [i for i in num_insignificant if i == KN][0]  # use
+        if KN in num_insignificant:
+            mhat = num_insignificant.index(KN)
         else:
-            if any(abs(rho_k) > rho_k_crit):
-                lag_sig = [i for i in abs(rho_k) > rho_k_crit if i]
-                k_sig = len(lag_sig)
-                if k_sig == 1:
-                    mhat = lag_sig
+            if any(np.abs(rho_k) > rho_k_crit):
+                lag_sig = np.where(rho_k > rho_k_crit)[0]
+                if len(lag_sig) == 1:
+                    mhat = lag_sig[0]
                 else:
-                    mhat = max(lag_sig)
+                    mhat = np.max(lag_sig)
             else:
                 mhat = 1
 
-        if 2 * mhat > mmax:
+        if 2 * (mhat + 1) > mmax:
             M = mmax
         else:
-            M = 2 * mhat
+            M = 2 * (mhat + 1)
 
-        kk = range(-M, M + 1)
+        kk = np.arange(-M, M + 1)
 
-        R_k = ccf(data)  # todo
+        acov = acovf(data)[: M + 1]
+        R_k = np.r_[acov[1:][::-1], acov]
 
+        Ghat = np.sum(lam(kk / float(M)) * np.abs(kk) * R_k)
+
+        if bootstrap_type == 'Circular':
+            Bhat = (4 / 3.) * np.sum(lam(kk / float(M)) * R_k) ** 2
+        else:
+            Bhat = 2 * np.sum(lam(kk / float(M)) * R_k) ** 2
+
+        def rounder(x):
+            if x < 1:
+                return 1
+            else:
+                if bootstrap_type == 'Circular':
+                    return np.round(x)
+                else:
+                    return np.ceil(x)
+
+        if not rnd:
+            bstar = min(((2 * Ghat ** 2) / Bhat) ** (1 / 3.) * n ** (1 / 3.), Bmax)
+        else:
+            bstar = min(rounder(((2 * Ghat ** 2) / Bhat) ** (1 / 3.) * n ** (1 / 3.)), Bmax)
+
+        Bstar.append(bstar)
+
+    return Bstar
 
 
 
@@ -188,12 +109,10 @@ def opt_block_length(data):
 
 
 if __name__ == '__main__':
-    df = pd.DataFrame([[0.3004800, 0.4806089, -1.0232758, 0.9733925, 0.2688148, -0.5161701, -0.7270535, -1.7454589,
-                        -0.1558424, -0.6287940],
-                       [0.3814848, 0.3526421, 0.8240130, 0.4161907, -0.6108061, -2.3804401, 1.4487400, 0.7112594,
-                        -0.4536392, 0.8210167]]).transpose()
-    opt_block_length(df)
+    # df = pd.DataFrame([[0.3004800, 0.4806089, -1.0232758, 0.9733925, 0.2688148, -0.5161701, -0.7270535, -1.7454589,
+    #                     -0.1558424, -0.6287940],
+    #                    [0.3814848, 0.3526421, 0.8240130, 0.4161907, -0.6108061, -2.3804401, 1.4487400, 0.7112594,
+    #                     -0.4536392, 0.8210167]]).transpose()
 
-
-
-#
+    df = pd.read_csv('/Users/travis.howe/Downloads/patton_x.csv', index_col=False).drop('Unnamed: 0', 1)
+    print opt_block_length(df, rnd=False)
