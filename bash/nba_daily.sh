5e0a6a8a04daa7c0a1e481e1597196d1739394c1
Travis Howe <b.travis.howe@gmail.com>
2017-12-07 07:26:12 -0600

    cleanup with bash reference sheets and nba daily shell script

--- bash/nba_daily.sh
+++ bash/nba_daily.sh
@@ -2,3 +2,5 @@ mkdir ~/Downloads/nba_daily
 python3 ../scrape/nba_scores_standings.py
 open ~/Downloads/nba_daily/nba_scoreboard.html
 open ~/Downloads/nba_daily/nba_standings.html
+open ~/Downloads/nba_daily/zach_lowe_articles.html
+open ~/Downloads/nba_daily/zach_lowe_podcast.html
--- scrape/nba_scores_standings.py
+++ scrape/nba_scores_standings.py
@@ -3,13 +3,15 @@ import datetime
 import requests
 
 my_dir = os.path.expanduser('~/Downloads/nba_daily')
-today = str(datetime.date.today()).replace('-', '')
-url = 'http://www.espn.com/nba/scoreboard/_/date/{}'.format(today)
-r = requests.get(url)
-with open(my_dir + '/nba_scoreboard.html', 'w') as my_file:
-    my_file.write(r.text)
+today = str(datetime.date.today() - datetime.timedelta(days=1)).replace('-', '')
+url_dict = {
+    '/nba_scoreboard.html': 'http://www.espn.com/nba/scoreboard/_/date/{}'.format(today),
+    '/nba_standings.html': 'http://www.espn.com/nba/standings',
+    '/zach_lowe_articles.html': 'https://muckrack.com/zachlowe_nba/articles',
+    '/zach_lowe_podcast.html': 'http://www.espn.com/espnradio/podcast/archive/_/id/10528553'
+}
 
-url = 'http://www.espn.com/nba/standings'
-r = requests.get(url)
-with open(my_dir + '/nba_standings.html', 'w') as my_file:
-    my_file.write(r.text)
+for filename, url in url_dict.items():
+    r = requests.get(url)
+    with open(my_dir + filename, 'w') as my_file:
+        my_file.write(r.text)
