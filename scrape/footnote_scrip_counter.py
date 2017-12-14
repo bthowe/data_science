d42e8e57f1d7408d26e750a1d891d3c896e9a92d
B. Travis Howe <b.travis.howe@gmail.com>
2017-12-14 00:19:12 -0600

    process footnote mongo data

--- scrape/footnote_scrip_counter.py
+++ scrape/footnote_scrip_counter.py
@@ -12,56 +12,55 @@ from pymongo import MongoClient
 # some type of classification of chapters based on the footnotes
 
 
-
-end_of_book_list = ['Moroni 10', 'Doctrine and Covenants 138', 'Articles of Faith 1', 'Malachi 4', 'Revelation 22']
-go = True
-
 client = MongoClient()
 db = client['scrip_footnotes']
 
-# for document in db.nt.find({'book': 'rev', 'chapter': '19'}):
-    # for reference in document['footnote'].split('; '):
-
-
 ab_lst = joblib.load('ab_lst.pkl')
 
-references = list(db.nt.find({'book': 'rev', 'chapter': '18'}))[-1]['footnote'].replace('\xa0', ' ')
-print(references)
-print('\n')
-book = ''
-for reference in references.split('; '):
-    rs = reference.split(' ')
-    if rs[0] in ab_lst:
-        book = rs[0]
-        ref = reference
-    else:
-        ref = '{0} {1}'.format(book, rs[0])
 
-
-    # if '(' in ref:
-    #
-    #     pass
-
-    print(ref)
-
-
-sys.exit()
-
-footnote_lst = []
-for collection in [db.bofm, db.dc-testament, db.pgp, db.ot, db.nt]:
-    for document in collection.find():
-        footnote = document['footnote'].replace('\xa0', ' ')
-#         todo: parse footnote
-#         todo: add to footnote_lst
-
-
-
-# todo: are there instances in which semi-colons do not delimit the references?
-#   -3 Nephi 27:21: There is a scrip followed by a TG reference, delimited by a period.
 class FootnoteParser(object):
     def __init__(self, footnote_str):
         self.footnote_str = footnote_str
 
-    def _prefix_set(self):
-        # todo: I wonder if it would instead be better to split based on the keyword abbreviations.
-        self.footnote_str.split('; ')
+    def _split_footnote(self, txt, seps):
+        default_sep = seps[0]
+
+        for sep in seps[1:]:
+            txt = txt.replace(sep, default_sep)
+        return [i.strip()[:-1] for i in txt.split(default_sep)][1:]
+
+    def _abs_in_footnote(self, txt, seps):
+        return [sep for sep in seps if sep in txt]
+
+    def references_add_prefix(self):
+        prefixes = self._abs_in_footnote(self.footnote_str, ab_lst)
+        suffixes = self._split_footnote(self.footnote_str, ab_lst)
+
+        ref_lst = []
+        for combos in itertools.product(prefixes, suffixes):
+            prefix = combos[0]
+            suffix = combos[1]
+
+            ref = ' '.join(combos)
+            if ref in self.footnote_str:
+                ref_lst += ['{0} {1}'.format(prefix, s) for s in suffix.split('; ')]
+        return ref_lst
+
+    def references_flatten(self, lst):
+        ref_lst = []
+        for ref in lst:
+            if '(' in lst:
+                pass  #todo: (1) take the numbers between the parentheses, (2) split by comma, (3) generate list
+            else:
+                ref_lst.append(ref)
+
+if __name__ == '__main__':
+    # for document in db['bofm'].find({'book': '3-ne', 'chapter': '27'}):
+    for document in db['nt'].find({'book': 'rev', 'chapter': '19'}):
+        footnote = document['footnote'].replace('\xa0', ' ')
+        fp = FootnoteParser(footnote)
+        print(fp.references_add_prefix())
+
+# todo: test this for other books, etc.
+#   -the jst is not correct---it uses two keywords (i.e., "jst" and "rev" (for example))
+# todo: polish the class---i.e., inputs, outputs, etc.
