e347b4f46dcd52ef30b691a80a29073498beef28
B. Travis Howe <b.travis.howe@gmail.com>
2017-12-15 23:10:52 -0600

    flatten, figure out

--- scrape/footnote_scrip_counter.py
+++ scrape/footnote_scrip_counter.py
@@ -32,9 +32,35 @@ class FootnoteParser(object):
     def _abs_in_footnote(self, txt, seps):
         return [sep for sep in seps if sep in txt]
 
+    def _flatten(self, lst):
+        ref_lst = []
+        for ref in lst.split('; '):
+            if '(' in lst:
+
+                verses = ref[ref.find('(') + 1: ref.find(')')]
+
+                verses_lst = []
+                for segs in verses.split(','):
+                    if '-' in segs:
+                        nums = segs.strip().split('-')
+                        seg_lst = map(str, list(range(int(nums[0]), int(nums[1]) + 1)))
+                    else:
+                        seg_lst = [segs]
+                    verses_lst += seg_lst
+                print(verses_lst)
+
+                pass  #todo: (1) take the numbers between the parentheses, (2) split by comma, (3) generate list
+            else:
+                ref_lst.append(ref)
+
+
     def references_add_prefix(self):
         prefixes = self._abs_in_footnote(self.footnote_str, ab_lst)
         suffixes = self._split_footnote(self.footnote_str, ab_lst)
+
+        # self._flatten(suffixes)
+        # print(suffixes);  sys.exit()
+
 
         ref_lst = []
         for combos in itertools.product(prefixes, suffixes):
@@ -43,24 +69,22 @@ class FootnoteParser(object):
 
             ref = ' '.join(combos)
             if ref in self.footnote_str:
-                ref_lst += ['{0} {1}'.format(prefix, s) for s in suffix.split('; ')]
+                ref_lst += ['{0} {1}'.format(prefix, s) for s in suffix.split('; ')]  # todo: can I just flatten each s if necessary:                 self._flatten(suffix)
+
         return ref_lst
 
-    def references_flatten(self, lst):
-        ref_lst = []
-        for ref in lst:
-            if '(' in lst:
-                pass  #todo: (1) take the numbers between the parentheses, (2) split by comma, (3) generate list
-            else:
-                ref_lst.append(ref)
+
 
 if __name__ == '__main__':
     # for document in db['bofm'].find({'book': '3-ne', 'chapter': '27'}):
-    for document in db['nt'].find({'book': 'rev', 'chapter': '19'}):
+    # for document in db['nt'].find({'book': 'rev', 'chapter': '19', 'verse': '2', 'letter': 'b'}):
+    for document in db['nt'].find({'book': 'rev', 'chapter': '18', 'letter': 'a', 'verse': '24'}):
         footnote = document['footnote'].replace('\xa0', ' ')
+        print(footnote)
         fp = FootnoteParser(footnote)
         print(fp.references_add_prefix())
 
+# todo: flatten
 # todo: test this for other books, etc.
 #   -the jst is not correct---it uses two keywords (i.e., "jst" and "rev" (for example))
 # todo: polish the class---i.e., inputs, outputs, etc.
