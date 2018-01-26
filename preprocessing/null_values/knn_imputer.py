1a20da8cc2e2727a1d15dc7fa534be9f6edfd548
Travis Howe <b.travis.howe@gmail.com>
2018-01-26 13:58:43 -0600

    rf similarity and rf imputer rough draft

--- preprocessing/null_values/knn_imputer.py
+++ preprocessing/null_values/knn_imputer.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import pandas as pd
+import scipy.stats as stats
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 
@@ -14,18 +15,17 @@ def rf_similarity(fitted_estimator, x, X):
     x: the point of comparison
     X_other: the data to compare to x
 
-    Returns the indeces of the most similar observations from X_other.
+    Returns a count for each data point in X of the number of trees in which it shares a leaf node with x
     """
     x_leafs = fitted_estimator.apply(x)
     X_leafs = fitted_estimator.apply(X)
 
     similarity = np.where(x_leafs == X_leafs, 1, 0).sum(axis=1)
-
-    indeces = np.argwhere(similarity == np.max(similarity)).flatten()
-    return X.index[indeces].values
+    return dict(zip(X.index, similarity))
 
 
 def rf_func(df, feature_to_impute, min_samples_leaf):
+    # todo: should I just use most similar points or allow user to specify the N most similar points
     """feature_to_impute: name of feature to fill the missing values"""
 
     df_to_impute = df.query('{0} != {0}'.format(feature_to_impute))
@@ -38,57 +38,15 @@ def rf_func(df, feature_to_impute, min_samples_leaf):
     rf = RandomForestClassifier(n_estimators=5, min_samples_leaf=min_samples_leaf)
     rf.fit(X, y)
 
+    rf_means = []
     for index in X.index:
         row = pd.DataFrame(X.loc[index]).transpose()
-        print(rf_similarity(rf, row, X.drop(index)))
+        count_dic = rf_similarity(rf, row, X.drop(index))
+        neighbors = list(filter(lambda x: count_dic[x] == max(count_dic.values()), count_dic.keys()))
+        rf_means.append(df.loc[neighbors][feature_to_impute].values.mean(axis=0))
+        # rf_means.append(df.loc[neighbors][estimate_cols].values.mean(axis=0))
 
-    sys.exit()
-
-
-
-
-
-
-    sys.exit()
-    print(X)
-    print(rf.apply(X))
-    a = rf.apply(X)
-    indeces = X.index.values.reshape((len(X), 1))
-
-
-    print(np.argmax(np.where(a == a[0, :], 1, 0).sum(axis=1)))
-
-
-
-    for index, row in enumerate(a):
-        similarity = np.where(row == a, 1, 0).sum(axis=1)
-        similarity[index] = -1
-
-        print(similarity)
-        print(np.argwhere(similarity == np.max(similarity)))
-
-        print(index)
-
-        sys.exit()
-        a_row = a[i, :]
-        a_rest = a
-
-        np.argwhere(list == np.amax(list))
-
-
-
-
-    # todo: go from this to some type of count of how often they are in the same group
-
-
-    sys.exit()
-
-
-    rfneighbors = rf.classes_  #.kneighbors(df_to_impute[estimate_cols])[1]
-
-    knn_means = [df.loc[neighbors][estimate_cols].values.mean() for neighbors in kneighbors]
-
-    df_to_impute[feature_to_impute] = knn_means
+    df_to_impute[feature_to_impute] = rf_means
 
     df.loc[df_to_impute.index] = df_to_impute
 
@@ -115,7 +73,12 @@ def knn_func(df, feature_to_impute, k):
 
     kneighbors = knn.kneighbors(df_to_impute[estimate_cols])[1]
 
-    knn_means = [df.loc[neighbors][estimate_cols].values.mean() for neighbors in kneighbors]
+    # todo: which of these is correct?
+    knn_means = [df.loc[neighbors][feature_to_impute].values.mean() for neighbors in kneighbors]
+    # knn_means = [df.loc[neighbors][estimate_cols].values.mean() for neighbors in kneighbors]
+
+    # todo: something is not right here
+    print(knn_means)
 
     df_to_impute[feature_to_impute] = knn_means
 
@@ -135,5 +98,5 @@ if __name__ == '__main__':
     df.loc[np.random.choice(20, size=2), 'one'] = np.nan
     df.loc[np.random.choice(20, size=2), 'two'] = np.nan
 
-    # knn_func(df, 'one', 3)
+    knn_func(df, 'one', 2)
     rf_func(df, 'one', 3)
