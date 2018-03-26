4c3587f46aa211cb7255685353747fd1702d4f0c
Travis Howe <b.travis.howe@gmail.com>
2018-03-26 10:32:35 -0500

    hierarchical model treatment effect

--- treatment effect evaluation/pairwise_randomized_experiments/bayesian_model_based_analysis.py
+++ treatment effect evaluation/pairwise_randomized_experiments/bayesian_model_based_analysis.py
@@ -30,8 +30,15 @@ def data_create():
     df_c['stratum'] = np.arange(N)
     df_c['treatment'] = 0
 
-    df = df_t.append(df_c)
-    df['mu_j'] = df.groupby(df['stratum']).apply(lambda x: np.random.normal(mu, sigma_mu))
+    df = df_t.append(df_c).reset_index(drop=True)
+
+    def func(x):
+        # x['mu_j'] = np.random.normal(mu, sigma_mu)
+        x['mu_j'] = np.random.normal(x.iloc[0]['stratum'] / 10, sigma_mu)
+        return x
+
+    df = df.groupby(df['stratum']).apply(func)
+    # df['mu_j'] = df.groupby(df['stratum']).apply(lambda x: np.random.normal(mu, sigma_mu))  # this doesn't work for some reason...the treatment=0 observations are null
 
     def y(x):
         if x['treatment'] == 1:
@@ -39,7 +46,9 @@ def data_create():
         else:
             return np.random.normal(beta * x['one'] + x['mu_j'], sigma_c)
     df['y'] = df.apply(y, axis=1)
-    return df
+
+    df = df.sample(frac=1, replace=False)
+    joblib.dump(df, '/Users/travis.howe/Downloads/df.pkl')
 
 def model_parameter_distribution_estimate(data):
     """
@@ -86,26 +95,49 @@ def posterior_predicted_distribution(trace, data):
     :return: posterior predicted distribution of the mean
     """
     # todo: how do I get the sub mu_js out of the trace?
-    #   -this must be done by index, determined by order of appearance. 
-
+    #   -this must be done by index, determined by order of appearance.
+    #   -it is not determined by order of appearance, but, rather, by order of strata
 
     # trace = trace_model[1000:]
 
+    # c_data = data.ix[data.county == c]
+    # c_data = c_data.reset_index(drop=True)
+    # c_index = np.where(county_names == c)[0][0]
+    # z = list(c_data['county_code'])[0]
 
-    c_data = data.ix[data.county == c]
-    c_data = c_data.reset_index(drop=True)
-    c_index = np.where(county_names == c)[0][0]
-    z = list(c_data['county_code'])[0]
+    # print(trace.varnames)
 
 
-    print(trace.varnames)
-    print(trace['mu_j']['0'])  #[1000:][z]
-    sys.exit()
 
-    lm = lambda x, samples: samples['mu_j'] + samples['gamma'] * x['treatment'] + samples['beta'] * x['one']
 
-    data['mu'] = [np.random.choice(lm(row[1], trace)) for row in data.iterrows()]
-    data['sigma'] = np.random.choice(trace['sd'], size=(len(data), 1), replace=True)
+
+    # print(np.mean(trace['mu_j'], axis=0).tolist())
+    # print(np.mean(trace['mu_j'], axis=0).shape)
+    #
+    # from scipy.stats import mode
+    # print(mode(trace['mu_j'], axis=0))
+    # sys.exit()
+    #
+    # pm.traceplot(trace)
+    # plt.show()
+    # sys.exit()
+
+
+    # todo: this is a mess
+
+    lm = lambda x, samples, ind: samples['mu_j'][:, ind] + samples['gamma'] * (1 - x['treatment']) + samples['beta'] * x['one']
+
+    y_est = []
+    sigma_est = []
+    for row in data.iterrows():
+        stratum = int(row[1]['stratum'])
+        treatment = (1 - int(row[1]['treatment']))
+
+        y_est.append(np.random.choice(lm(row[1], trace, stratum)))
+        sigma_est.append(np.random.choice(trace['sigma'][:, treatment]))
+
+    data['y_missing_est'] = y_est
+    data['sigma_missing'] = sigma_est
 
     return data
 
@@ -113,10 +145,11 @@ def missing_draws(df):
     """
     Draws from the distribution of the missing data conditional on the observed data and the parameters.
     """
-    df['target_missing'] = norm.rvs(df['mu'], df['sigma'])
+    df['target_missing'] = norm.rvs(df['y_missing_est'], df['sigma_missing'])
     return df
 
 def outcomes_create(df):
+    df.rename(columns={'y': 'target'}, inplace=True)
     df['y_control'] = df.apply(lambda x: x['target'] if x['treatment'] == 0 else x['target_missing'], axis=1)
     df['y_treatment'] = df.apply(lambda x: x['target'] if x['treatment'] == 1 else x['target_missing'], axis=1)
     return df
@@ -127,12 +160,21 @@ def treatment_effect_calc(df):
 
 
 if __name__ == '__main__':
-    df = data_create()
+    # data_create()
+
+    df = joblib.load('/Users/travis.howe/Downloads/df.pkl')
     # model_parameter_distribution_estimate(df)
 
     trace = joblib.load('/Users/travis.howe/Downloads/trace.pkl')
-    posterior_predicted_distribution(trace, df)
+    t_fs = posterior_predicted_distribution(trace, df).\
+        pipe(missing_draws).\
+        pipe(outcomes_create).\
+        pipe(treatment_effect_calc)
+    print(t_fs)
 
+
+# todo: make sure things are correct here and clean up the messy code
 
 # todo: https://en.wikipedia.org/wiki/Inverse-gamma_distribution; inverse chi 2 can be cast as an inverse gamma
 # todo: how do you go from the traces to the treatment effect? In finite sample, the same routine as above applies, whereas in super population, the posterior of gamma is what I want.
+
