e94d0e9956a916f548bad9a5cd698f2d3b0e5ef4
Travis Howe <b.travis.howe@gmail.com>
2018-04-30 07:08:32 -0500

    extra trees added

--- estimators/machine learning/regression/extra_trees_quantile_regression.py
+++ estimators/machine learning/regression/extra_trees_quantile_regression.py
@@ -1,8 +1,9 @@
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_boston
-from skgarden import RandomForestQuantileRegressor
+from skgarden import RandomForestQuantileRegressor, ExtraTreesQuantileRegressor
 from sklearn.model_selection import train_test_split
 
 def data_create():
@@ -12,7 +13,8 @@ def data_create():
     return train_test_split(X, y, test_size=.3, random_state=22)
 
 def model_train(X_train, y_train):
-    rfqr = RandomForestQuantileRegressor(random_state=0, min_samples_split=10, n_estimators=100)
+    # rfqr = RandomForestQuantileRegressor(random_state=0, min_samples_split=10, n_estimators=100)
+    rfqr = ExtraTreesQuantileRegressor(random_state=0, min_samples_split=10, n_estimators=100)
     rfqr.fit(X_train, y_train)
     return rfqr
 
@@ -21,6 +23,7 @@ def interval_predict(model, X_test, y_test):
         [
             pd.DataFrame(y_test, columns=['actual']),
             pd.DataFrame(model.predict(X_test, quantile=97.5), columns=['quantile_97.5']),
+            pd.DataFrame(model.predict(X_test, quantile=50), columns=['median']),
             pd.DataFrame(model.predict(X_test, quantile=2.5), columns=['quantile_2.5'])
         ],
         axis=1
@@ -29,6 +32,7 @@ def interval_predict(model, X_test, y_test):
     df['actual'] = df['actual'] - mean
     df['quantile_97.5'] = df['quantile_97.5'] - mean
     df['quantile_2.5'] = df['quantile_2.5'] - mean
+    df['median'] = df['median'] - mean
     return df
 
 def plotter(df):
@@ -44,8 +48,10 @@ def plotter(df, ordered=False):
         df.sort_values('diff', inplace=True)
     fig = plt.figure(figsize=(12, 8))
     ax = fig.add_subplot(1, 1, 1)
-    ax.scatter(range(len(df)), df['actual'], color='red')
+    ax.scatter(range(len(df)), df['actual'], color='red', label='Actual')
     ax.fill_between(range(len(df)), df['quantile_2.5'], df['quantile_97.5'], alpha=0.2, color='r')
+    ax.scatter(range(len(df)), df['median'], color='blue', alpha=.4, label='Median')
+    plt.legend()
     plt.show()
 
 if __name__ == '__main__':
