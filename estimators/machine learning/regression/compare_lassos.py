cd329d8fff0a7ea6e5310285fba9441263f9de43
Travis Howe <b.travis.howe@gmail.com>
2018-05-10 08:36:37 -0500

    different data creation

--- estimators/machine learning/regression/compare_lassos.py
+++ estimators/machine learning/regression/compare_lassos.py
@@ -1,9 +1,13 @@
 import sys
 import numpy as np
 import pandas as pd
-from scipy.stats import binom
 import matplotlib.pyplot as plt
+from scipy.stats import uniform
+from scipy.stats import norm
+from sklearn.metrics.scorer import make_scorer
+from sklearn.metrics import mean_absolute_error
 from sklearn.linear_model import MultiTaskLasso, Lasso
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 
 pd.set_option('max_columns', 1000)
 pd.set_option('max_info_columns', 1000)
@@ -13,40 +17,135 @@ pd.set_option('max_colwidth', 4000)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
 
-rng = np.random.RandomState(42)
-
 def data_create():
-    N = 1000
+    np.random.seed(seed=2)
 
-    beta0 = -1.6
-    beta1 = -0.03
-    beta2 = 0.6
-    beta3 = 1.6
+    n_obs = 1000
+    n_outcomes = 3
+    n_features = 1
 
-    df = pd.DataFrame(np.random.uniform(-1, 1, size=(N, 3)), columns=['X1', 'X2', 'X3'])
-    df['mu'] = beta0 + beta1 * df['X1'] + beta2 * df['X2'] + beta3 * df['X3']
-    df['y1'] = df['mu'] + np.random.normal(0, 1, size=(N,))
-    df['y2'] = df['mu'] + 1 + np.random.normal(0, 1, size=(N,))
-    df['y3'] = df['mu'] + .5 + np.random.normal(0, 1, size=(N,))
+    beta = np.array([-1.6, -0.03])
+    # beta = np.array([-1.6, -0.03, 0.6, 1.6])
 
-    df['X0'] = 1
+    coefs = np.zeros((n_features + 1, n_outcomes))
+    for coef in range(n_outcomes):
+        coefs[:, coef] = beta + norm.rvs(0, 1, size=(n_features + 1, ))
+
+    df = pd.concat(
+        [
+            pd.DataFrame(np.ones((n_obs, 1)), columns=['X0']),
+            pd.DataFrame(np.random.uniform(-1, 1, size=(n_obs, n_features)), columns=['X1'])
+            # pd.DataFrame(np.random.uniform(-1, 1, size=(n_obs, n_features)), columns=['X1', 'X2', 'X3'])
+        ],
+        axis=1
+    )
+    X = df.values
+    df[['y1', 'y2', 'y3']] = pd.DataFrame(np.dot(X, coefs)) + norm.rvs(0, 1, size=(n_obs, n_outcomes))
     return df
 
+def mtl_roc_auc(y_true, y_pred):
+    return mean_absolute_error(y_true.values.flatten(), y_pred.flatten())
+
 def multi_task_lasso(df):
-    X = df[['X0', 'X1', 'X2', 'X3']]
+    X = df[['X0', 'X1']]
+    # X = df[['X0', 'X1', 'X2', 'X3']]
     Y = df[['y1', 'y2', 'y3']]
 
-    # coef_lasso_ = np.array([Lasso(alpha=0.5).fit(X, y).coef_ for y in Y.T])
+    mtl_scorer = make_scorer(mtl_roc_auc, greater_is_better=True)
+    mtl_parameters = {
+        'alpha': uniform(0, 10)
+    }
+
+    grid_search = RandomizedSearchCV(
+        MultiTaskLasso(fit_intercept=False, alpha=0.05),
+        mtl_parameters,
+        n_iter=200,
+        scoring=mtl_scorer,
+        verbose=10,
+        n_jobs=1,
+        cv=5
+    )
+    grid_search.fit(X, Y)
+    print(grid_search.best_params_)
+    print(grid_search.best_score_)
+    print(grid_search.best_estimator_.coef_)
 
 
+def plotter(df):
+    fig = plt.figure(figsize=(12, 8))
+    ax = fig.add_subplot(3, 1, 1)
+    ax.scatter(df['X1'], df['y1'], color='seagreen', alpha=.2)
+    ax.plot(df['X1'], df['y1_predict'], color='seagreen')
+    ax = fig.add_subplot(3, 1, 2)
+    ax.scatter(df['X1'], df['y2'], color='cornflowerblue', alpha=.2)
+    ax.plot(df['X1'], df['y2_predict'], color='cornflowerblue')
+    ax = fig.add_subplot(3, 1, 3)
+    ax.scatter(df['X1'], df['y3'], color='darksalmon', alpha=.2)
+    ax.plot(df['X1'], df['y3_predict'], color='darksalmon')
+    plt.show()
 
-    print(np.array([Lasso(fit_intercept=False, alpha=0.05).fit(X, Y[y]).coef_ for y in Y]))
 
-    print(MultiTaskLasso(fit_intercept=False, alpha=0.05).fit(X, Y).coef_)
+def lasso(df):
+    X = df[['X0', 'X1']]
+    # X = df[['X0', 'X1', 'X2', 'X3']]
+    Y = df[['y1', 'y2', 'y3']]
+
+    from scipy.stats import expon
+    l_parameters_rs = {
+        'alpha': expon()
+    }
+    # l_parameters_rs = {
+    #     'alpha': uniform(0, 10)
+    # }
+
+    l_parameters_gs = {
+        'alpha': [0.01, 0.1, 0, 1, 10]
+    }
+
+    for y in Y:
+        # l = Lasso(fit_intercept=False, alpha=0.0)
+        # l.fit(X, Y[y])
+        # df['{}_predict'.format(y)] = l.predict(X)
+        # print(l.coef_)
+
+        grid_search = RandomizedSearchCV(
+            Lasso(fit_intercept=False, alpha=0.0),
+            l_parameters_rs,
+            n_iter=200,
+            scoring='neg_mean_squared_error',
+            verbose=0,
+            n_jobs=-1,
+            cv=5
+        )
+        grid_search.fit(X, Y[y])
+        print(grid_search.best_params_)
+        print(grid_search.best_score_)
+        df['{}_predict'.format(y)] = grid_search.predict(X)
+        print(grid_search.best_estimator_.coef_)
+        #
+        # grid_search = GridSearchCV(
+        #     Lasso(fit_intercept=False, alpha=0.0),
+        #     l_parameters_gs,
+        #     scoring='neg_mean_squared_error',
+        #     verbose=0,
+        #     n_jobs=-1,
+        #     cv=5
+        # )
+        # grid_search.fit(X, Y[y])
+        # print(grid_search.best_params_)
+        # print(grid_search.best_score_)
+        # df['{}_predict'.format(y)] = grid_search.predict(X)
+        # print(grid_search.best_estimator_.coef_)
+
+    # print(np.array([Lasso(fit_intercept=False, alpha=0.0).fit(X, Y[y]).coef_ for y in Y]))
+
+    return df
+    # print(MultiTaskLasso(fit_intercept=False, alpha=0.05).fit(X, Y).coef_)
 
 if __name__ == '__main__':
-    df = data_create().pipe(multi_task_lasso)
-
+    # df = data_create().pipe(plotter)
+    # df = data_create().pipe(multi_task_lasso)
+    data_create().pipe(lasso).pipe(plotter)
 
 # todo: gridsearch
 # todo: compare fits and coefficients
--- estimators/machine learning/regression/multi_task_lasso.py
+++ estimators/machine learning/regression/multi_task_lasso.py
@@ -9,49 +9,58 @@ from sklearn.linear_model import MultiTaskLasso, Lasso
 
 rng = np.random.RandomState(42)
 
-# Generate some 2D coefficients with sine waves with random frequency and phase
-n_samples, n_features, n_tasks = 100, 30, 40
-n_relevant_features = 5
-coef = np.zeros((n_tasks, n_features))
-times = np.linspace(0, 2 * np.pi, n_tasks)
-for k in range(n_relevant_features):
-    coef[:, k] = np.sin((1. + rng.randn(1)) * times + 3 * rng.randn(1))
+def data_create():
+    n_obs, n_features, n_tasks = 100, 30, 40
+    n_relevant_features = 5
 
-X = rng.randn(n_samples, n_features)
-print(X.shape)
-print(np.dot(X, coef.T).shape)
-print(rng.randn(n_samples, n_tasks).shape)
-sys.exit()
-Y = np.dot(X, coef.T) + rng.randn(n_samples, n_tasks)  # (100 x 30) * (30 x 40), which is the linear combination of features * coefficients
+    coef = np.zeros((n_tasks, n_features))
+    times = np.linspace(0, 2 * np.pi, n_tasks)
+    # print(times)
+    # print(1 + rng.randn(1))
+    # print(np.sin((1 + rng.randn(1))) * times)
+    # sys.exit()
+    for k in range(n_relevant_features):
+        coef[:, k] = np.sin((1 + rng.randn(1)) * times + 3 * rng.randn(1))
 
-coef_lasso_ = np.array([Lasso(alpha=0.5).fit(X, y).coef_ for y in Y.T])
-coef_multi_task_lasso_ = MultiTaskLasso(alpha=1.).fit(X, Y).coef_
+    X = rng.randn(n_obs, n_features)
+    Y = np.dot(X, coef.T) + rng.randn(n_obs, n_tasks)  # (100 x 30) * (30 x 40), which is the linear combination of features * coefficients
 
-# #############################################################################
-# Plot support and time series
-fig = plt.figure(figsize=(8, 5))
-plt.subplot(1, 2, 1)
-plt.spy(coef_lasso_)
-plt.xlabel('Feature')
-plt.ylabel('Time (or Task)')
-plt.text(10, 5, 'Lasso')
-plt.subplot(1, 2, 2)
-plt.spy(coef_multi_task_lasso_)
-plt.xlabel('Feature')
-plt.ylabel('Time (or Task)')
-plt.text(10, 5, 'MultiTaskLasso')
-fig.suptitle('Coefficient non-zero location')
+    fig = plt.figure(figsize=(12, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(X[:, 0], Y[:, 0])
+    plt.show()
 
-feature_to_plot = 0
-plt.figure()
-lw = 2
-plt.plot(coef[:, feature_to_plot], color='seagreen', linewidth=lw,
-         label='Ground truth')
-plt.plot(coef_lasso_[:, feature_to_plot], color='cornflowerblue', linewidth=lw,
-         label='Lasso')
-plt.plot(coef_multi_task_lasso_[:, feature_to_plot], color='gold', linewidth=lw,
-         label='MultiTaskLasso')
-plt.legend(loc='upper center')
-plt.axis('tight')
-plt.ylim([-1.1, 1.1])
-plt.show()
+def muti_task_lasso(df):
+    coef_lasso_ = np.array([Lasso(alpha=0.5).fit(X, y).coef_ for y in Y.T])
+    coef_multi_task_lasso_ = MultiTaskLasso(alpha=1.).fit(X, Y).coef_
+
+def plot_predictions(df):
+    fig = plt.figure(figsize=(8, 5))
+    plt.subplot(1, 2, 1)
+    plt.spy(coef_lasso_)
+    plt.xlabel('Feature')
+    plt.ylabel('Time (or Task)')
+    plt.text(10, 5, 'Lasso')
+    plt.subplot(1, 2, 2)
+    plt.spy(coef_multi_task_lasso_)
+    plt.xlabel('Feature')
+    plt.ylabel('Time (or Task)')
+    plt.text(10, 5, 'MultiTaskLasso')
+    fig.suptitle('Coefficient non-zero location')
+
+    feature_to_plot = 0
+    plt.figure()
+    lw = 2
+    plt.plot(coef[:, feature_to_plot], color='seagreen', linewidth=lw,
+             label='Ground truth')
+    plt.plot(coef_lasso_[:, feature_to_plot], color='cornflowerblue', linewidth=lw,
+             label='Lasso')
+    plt.plot(coef_multi_task_lasso_[:, feature_to_plot], color='gold', linewidth=lw,
+             label='MultiTaskLasso')
+    plt.legend(loc='upper center')
+    plt.axis('tight')
+    plt.ylim([-1.1, 1.1])
+    plt.show()
+
+if __name__ == '__main__':
+    data_create()
