da028d3a5dfe654c550abed00b2eb86eac6d7c70
Travis Howe <b.travis.howe@gmail.com>
2018-06-08 14:17:09 -0500

    n_periods forecasts plot

--- estimators/tensorflow/rec_neural_net/dynamic_time_series.py
+++ estimators/tensorflow/rec_neural_net/dynamic_time_series.py
@@ -57,11 +57,10 @@ class SequenceClassification(object):
         return tf.layers.dense(outputs_reshaped, 20)
     #     todo: why does the latter perform so much better than the former?
 
-
     @lazy_property
     def cost(self):
+        # print(self.prediction)
         return tf.reduce_mean(tf.abs(self.prediction - self.target))
-
 
     @lazy_property
     def optimize(self):
@@ -69,13 +68,13 @@ class SequenceClassification(object):
         optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
         return optimizer.minimize(self.cost)
 
-
     @lazy_property
     def error(self):
         return self.cost
 
 
 def data_create():
+    np.random.seed(42)
     t = np.linspace(0, 6 * np.pi, 1021)
     y = np.sin(t) + np.random.normal(0, .1, size=(1021,))
 
@@ -99,7 +98,6 @@ def _get_next_batch(batch_size, time_steps, X, y, test=False):
         y_batch[:, t] = [y[i + t] for i in batch_id]
 
     return np.expand_dims(x_batch, axis=3), y_batch
-    # return np.expand_dims(x_batch, axis=3), np.expand_dims(y_batch, axis=3)
 
 
 def fit_plotter(y_pred, y_true):
@@ -120,14 +118,12 @@ def main():
     # construction
     n_steps = 20
     n_inputs = 1
-    n_outputs = 1
     n_epochs = 100
     batch_size = 50
 
-    data = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+    data = tf.placeholder(tf.float32, [None, n_steps, n_inputs], name='data')
     target = tf.placeholder(tf.float32, [None, n_steps])
-    # target = tf.placeholder(tf.float32, [None, n_steps, n_outputs])
-    dropout = tf.placeholder(tf.float32)
+    dropout = tf.placeholder(tf.float32, name='dropout')
     model = SequenceClassification(data, target, dropout)
 
     X_test, y_test = _get_next_batch(1, n_steps, X_test, y_test, test=True)  # prep test data so don't have to do it every iteration
@@ -144,9 +140,50 @@ def main():
         test_acc = sess.run(model.error, {data: X_test, target: y_test, dropout: 1})
         print('Epoch {:2d} train mae {:3.5f}, test mae {:3.5f}'.format(epoch + 1, train_acc, test_acc))
 
+    tf.train.Saver().save(sess, './ts_model')
+
     y_pred = sess.run(model.prediction, {data: X_test, dropout: 1}).flatten()
     fit_plotter(y_pred, y_test.flatten())
 
 
+def forecast(periods):
+    X_train, X_test, y_train, y_test = data_create()
+    X = X_train[-20:, :]  # remember this 20 here and below is a hyperparameter in the model.
+
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph('ts_model.meta')
+        saver.restore(sess, tf.train.latest_checkpoint('./'))
+
+        graph = tf.get_default_graph()
+        pred = graph.get_tensor_by_name('dense/BiasAdd:0')
+        data = graph.get_tensor_by_name('data:0')
+        dropout = graph.get_tensor_by_name('dropout:0')
+
+        forecasts = []
+        for _ in range(periods):
+            X_batch, y_batch = _get_next_batch(1, 20, X, y_train[-20:], test=True)
+            next_pred = sess.run(pred, {data: X_batch, dropout: 1}).flatten()[-1]
+            forecasts.append(next_pred)
+            X = np.r_[X[1:, :], [[next_pred]]]
+
+    # fit_plotter(X.flatten()[-periods:], y_test.flatten()[:periods])
+    # print('MAE of forecasts: {0:.3f}'.format(np.mean(np.abs(X.flatten()[-periods:] - y_test.flatten()[:periods]))))
+    return forecasts
+
+
+def full_data_plotter(forecast_periods):
+    X_train, X_test, y_train, y_test = data_create()
+
+    y_forecast = forecast(forecast_periods)
+
+    fig = plt.figure(figsize=(12, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(range(len(X_train)), X_train, color='cornflowerblue')
+    ax.plot(range(len(X_train), len(X_train) + len(X_test)), X_test, color='seagreen')
+    ax.plot(range(len(X_train), len(X_train) + forecast_periods), y_forecast, color='firebrick')
+    plt.show()
+
 if __name__ == '__main__':
-    main()
+    # main()
+    full_data_plotter(20)
+
---
+++ estimators/tensorflow/rec_neural_net/sequence_generate.py
@@ -0,0 +1,95 @@
+# Adapted from https://gist.github.com/danijar/c7ec9a30052127c7a1ad169eeb83f159
+import sys
+import functools
+import sets
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def lazy_property(function):
+    attribute = '_' + function.__name__
+
+    @property
+    @functools.wraps(function)
+    def wrapper(self):
+        if not hasattr(self, attribute):
+            setattr(self, attribute, function(self))
+        return getattr(self, attribute)
+    return wrapper
+
+
+class SequenceClassification(object):
+    def __init__(self, data, target, dropout, num_classes, num_hidden=200, num_layers=3):
+        self.data = data
+        self.target = target
+        self.dropout = dropout
+        self._num_classes = num_classes
+        self._num_hidden = num_hidden
+        self._num_layers = num_layers
+        self.prediction
+        self.error
+        self.optimize
+
+    @lazy_property
+    def prediction(self):
+        def make_cell():
+            # return tf.contrib.rnn.GRUCell(num_units=self._num_hidden)
+            return tf.contrib.rnn.DropoutWrapper(
+                tf.contrib.rnn.BasicRNNCell(num_units=self._num_hidden),
+                output_keep_prob=self.dropout
+            )
+
+        network = tf.contrib.rnn.MultiRNNCell([make_cell() for _ in range(self._num_layers)])
+        outputs, states = tf.nn.dynamic_rnn(network, self.data, dtype=tf.float32)
+
+        outputs_reshaped = tf.reshape(outputs, [-1, self._num_hidden * self.data.shape[1]])  # nodes in hidden layer times the number of features which is the size of the second dimension
+        return tf.layers.dense(outputs_reshaped, self._num_classes)
+
+    @lazy_property
+    def cost(self):
+        xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.target, logits=self.prediction)
+        return tf.reduce_mean(xentropy)
+
+    @lazy_property
+    def optimize(self):
+        learning_rate = 0.001
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        return optimizer.minimize(self.cost)
+
+    @lazy_property
+    def error(self):
+        correct = tf.nn.in_top_k(self.prediction, self.target, 1)
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+        return accuracy
+
+
+def main():
+    # # We treat images as sequences of pixel rows.
+    rows = 28
+    row_size = 28
+    num_classes = 10
+    n_epochs = 100
+    batch_size = 150
+
+    mnist = input_data.read_data_sets('/tmp/data/')
+    X_test = mnist.test.images.reshape((-1, rows, row_size))
+    y_test = mnist.test.labels
+
+    data = tf.placeholder(tf.float32, [None, rows, row_size])
+    target = tf.placeholder(tf.int32, [None])
+    dropout = tf.placeholder(tf.float32)
+    model = SequenceClassification(data, target, dropout, num_classes)
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    for epoch in range(n_epochs):
+        for iteration in range(mnist.train.num_examples // batch_size):
+            X_batch, y_batch = mnist.train.next_batch(batch_size)
+            X_batch = X_batch.reshape((-1, rows, row_size))
+            sess.run(model.optimize, {data: X_batch, target: y_batch, dropout: 0.5})
+        train_acc = sess.run(model.error, {data: X_batch, target: y_batch, dropout: 1})
+        test_acc = sess.run(model.error, {data: X_test, target: y_test, dropout: 1})
+        print('Epoch {:2d} train accuracy {:3.5f}, test accuracy {:3.5f}'.format(epoch + 1, train_acc, test_acc))
+
+if __name__ == '__main__':
+    main()
