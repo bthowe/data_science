102ee18c893194e4fe6f3c317e6a5613813a61aa
Travis Howe <b.travis.howe@gmail.com>
2018-06-27 22:42:13 -0500

    cart pole train and refactor

--- estimators/tensorflow/reinforcement_learning/cart_pole_refactor.py
+++ estimators/tensorflow/reinforcement_learning/cart_pole_refactor.py
@@ -1,4 +1,5 @@
 import sys
+import functools
 import numpy as np
 import tensorflow as tf
 from tensorflow.python import debug as tf_debug
@@ -6,25 +7,63 @@ from tensorflow.python import debug as tf_debug
 import gym
 env = gym.make('CartPole-v0').env
 
-def train():
+
+def lazy_property(function):
+    attribute = '_' + function.__name__
+
+    @property
+    @functools.wraps(function)
+    def wrapper(self):
+        if not hasattr(self, attribute):
+            setattr(self, attribute, function(self))
+        return getattr(self, attribute)
+    return wrapper
+
+
+class SequenceClassification(object):
+    def __init__(self, data, initalizer, num_outputs, num_hidden):
+        self.data = data
+        self.initializer = initalizer
+        self._num_outputs = num_outputs
+        self._num_hidden = num_hidden
+
+        self.prediction
+        self.target
+        self.optimize
+
+    @lazy_property
+    def prediction(self):
+        hidden = tf.layers.dense(self.data, self._num_hidden, activation=tf.nn.elu, kernel_initializer=self.initializer)
+        return tf.layers.dense(hidden, self._num_outputs, kernel_initializer=self.initializer)
+
+    @lazy_property
+    def target(self):
+        outputs = tf.nn.sigmoid(self.prediction, name='outputs')
+        p_left_and_right = tf.concat(axis=1, values=[outputs, 1 - outputs], name='p_left_and_right')
+        return tf.multinomial(tf.log(p_left_and_right), num_samples=1, name='action')
+        # return 1. - tf.to_float(action)
+
+    @lazy_property
+    def cost(self):
+        return tf.nn.sigmoid_cross_entropy_with_logits(labels=1. - tf.to_float(self.target), logits=self.prediction)
+
+    @lazy_property
+    def optimize(self):
+        learning_rate = 0.001
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        return self.optimizer.compute_gradients(self.cost)
+
+
+def main():
     n_inputs = 4
-    n_hidden = 4
     n_outputs = 1
+    n_hidden = 4
     initializer = tf.contrib.layers.variance_scaling_initializer()
 
-    learning_rate = 0.01
     X = tf.placeholder(tf.float32, shape=[None, n_inputs], name='X')
-    hidden = tf.layers.dense(X, n_hidden, activation=tf.nn.elu, kernel_initializer=initializer)
-    logits = tf.layers.dense(hidden, n_outputs, kernel_initializer=initializer)
-    outputs = tf.nn.sigmoid(logits, name='outputs')
+    model = SequenceClassification(X, initializer, n_outputs, n_hidden)
 
-    p_left_and_right = tf.concat(axis=1, values=[outputs, 1 - outputs], name='p_left_and_right')
-    action = tf.multinomial(tf.log(p_left_and_right), num_samples=1, name='action')
-
-    y = 1. - tf.to_float(action)
-    cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=logits)
-    optimizer = tf.train.AdamOptimizer(learning_rate)
-    grads_and_vars = optimizer.compute_gradients(cross_entropy)
+    grads_and_vars = model.optimize
     gradients = [grad for grad, variable in grads_and_vars]
     gradient_placeholders = []
     grads_and_vars_feed = []
@@ -32,7 +71,7 @@ def train():
         gradient_placeholder = tf.placeholder(tf.float32, shape=grad.get_shape())
         gradient_placeholders.append(gradient_placeholder)
         grads_and_vars_feed.append((gradient_placeholder, variable))
-    training_op = optimizer.apply_gradients(grads_and_vars_feed)
+    training_op = model.optimizer.apply_gradients(grads_and_vars_feed)
 
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
@@ -53,8 +92,7 @@ def train():
         reward_std = flat_rewards.std()
         return [(discounted_rewards - reward_mean) / reward_std for discounted_rewards in all_discounted_rewards]
 
-    n_iterations = 1000
-    # n_iterations = 250
+    n_iterations = 2000
     n_max_steps = 1000
     n_games_per_update = 10
     save_iterations = 10
@@ -73,7 +111,7 @@ def train():
 
                 obs = env.reset()
                 for step in range(n_max_steps):
-                    action_val, gradients_val = sess.run([action, gradients], feed_dict={X: obs.reshape(1, n_inputs)})
+                    action_val, gradients_val = sess.run([model.target, gradients], feed_dict={X: obs.reshape(1, n_inputs)})
                     obs, reward, done, info = env.step(action_val[0][0])
                     current_rewards.append(reward)
                     current_gradients.append(gradients_val)
@@ -94,14 +132,14 @@ def train():
                 feed_dict[grad_placeholder] = mean_gradients
             sess.run(training_op, feed_dict=feed_dict)
             if iteration % save_iterations == 0:
-                saver.save(sess, "./cart_pole.ckpt")
+                saver.save(sess, "./my_policy_net_pg.ckpt")
 
     env.close()
 
 def test():
     n_inputs = 4
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph('cart_pole.ckpt.meta')
+        saver = tf.train.import_meta_graph('my_policy_net_pg.ckpt.meta')
         saver.restore(sess, tf.train.latest_checkpoint('./'))
 
         graph = tf.get_default_graph()
@@ -130,5 +168,8 @@ def test():
 
 
 if __name__ == '__main__':
-    train()
+    # main()
     test()
+
+
+# todo: this is not updating like it should
