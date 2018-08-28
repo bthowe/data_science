f431e706e7d9ab37997f96286faf428d6b63af89
Travis Howe <b.travis.howe@gmail.com>
2018-08-28 13:37:34 -0500

    propensity score specification class draft

--- causal_inference/I_and_R_treatment effect evaluation/propensity_score_specification.py
+++ causal_inference/I_and_R_treatment effect evaluation/propensity_score_specification.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import roc_auc_score
 from sklearn.feature_selection import RFE
+from itertools import combinations_with_replacement
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.metrics import log_loss
@@ -22,44 +23,95 @@ def _feature_keep(df):
     covars = ['treatment_group', 'monthly_premium', 'agent_tenure', 'policy_wait', 'state_id', 'payment_time']
     return df[covars]
 
+
 def _feature_dummy_create(df):
     df['state_id'] = df['state_id'].astype(str)
     return pd.get_dummies(df)
 
+
 def preprocess(df):
     return df.pipe(_feature_keep).pipe(_feature_dummy_create)
 
-def additional_linear_terms(df, basic_covars, t_true):
-    lr = LogisticRegression()
-    lr.fit(df[basic_covars], t_true)
-    t_pred = lr.predict_proba(df[basic_covars])
-    l0 = log_loss(t_true, t_pred)
 
-    other_covars = [covar for covar in df if covar not in basic_covars]
-    score = {'score': 0, 'covar': ''}
-    for covar in other_covars:
-        new_covars = basic_covars + [covar]
-        lr = LogisticRegression()
-        lr.fit(df[new_covars], t_true)
-        t_pred = lr.predict_proba(df[new_covars])
-        l1 = log_loss(t_true, t_pred)
+class PropensityScoreSpecification(object):
+    """The purpose of this class is to (1) algorithmically identify a specification useful for estimating the
+    propensity score, and (2) estimate the propensity score for each observations."""
+    def __init__(self, X, t, basic_covars, linear_thresh=1, quad_thresh=2.71):
+        self.X = X
+        self.t = t
+        self.basic_covars = basic_covars
+        self.linear_thresh = linear_thresh
+        self.quad_thresh = quad_thresh
 
-        new_score = l0 / l1
-        if score['score'] < new_score:
-            score['score'] = new_score
-            score['covar'] = covar
-    print(score)
-    sys.exit()
+        self.X_update = self.X[basic_covars]
+        self.covar_lst = None
 
+        self._candidate_covars = []
+        self._lr = LogisticRegression()
+        self._l0 = self._likelihood_calc(self.X_update)
 
-def quadratic_interaction_terms(df):
-    pass
+    def _likelihood_calc(self, X):
+        self._lr.fit(X, self.t)
+        t_pred = self._lr.predict_proba(X)
+        return log_loss(self.t, t_pred)
+
+    def _choose_covar(self, method):
+        score = {'score': 0, 'covar': '', 'data': None}
+        for covar in self._candidate_covars:
+            if method == 'quad':
+                X_new = self.X_update[covar.split('__X__')[0]] * self.X_update[covar.split('__X__')[1]]
+            else:
+                X_new = self.X[covar]
+            X_temp = pd.concat(
+                [
+                    self.X_update,
+                    X_new
+                ], axis=1
+            )
+            l1 = self._likelihood_calc(X_temp)
+
+            new_score = self._l0 / l1
+            if score['score'] < new_score:
+                score['score'] = new_score
+                score['covar'] = covar
+                score['data'] = X_temp
+        return score
+
+    def _iterate(self, method, thresh):
+        go = True
+        while go:
+            score = self._choose_covar(method)  # find the covar with the highest lr statistic
+            if score['score'] > thresh:  # this score must be greater than the threshold
+                self.X_update = score['data']  # update the baseline dataset
+                self._l0 = self._likelihood_calc(self.X_update)  # update the baseline likelihood
+                self._candidate_covars.remove(score['covar'])  # remove the covariate from list of candidates
+            else:
+                go = False
+            if not self._candidate_covars:
+                go = False
+
+    def _additional_linear_terms(self):
+        self._candidate_covars = [col for col in self.X if col not in self.basic_covars]
+        self._iterate('linear', self.linear_thresh)
+
+    def _quadratic_interaction_terms(self):
+        self._candidate_covars = ['__X__'.join(pair) for pair in combinations_with_replacement(self.X_update.columns, 2)]
+        self._iterate('quad', self.quad_thresh)
+
+    def specification_create(self):
+        self._additional_linear_terms()
+        self._quadratic_interaction_terms()
+        self.covar_lst = self.X_update.columns.tolist()
+        return self
+
+    def score(self):
+        self._lr.fit(self.X_update, self.t)
+        return self._lr.predict_proba(self.X_update)[:, 0]
 
 if __name__ == '__main__':
     df = pd.read_csv('test_data.csv').pipe(preprocess)
     t = df.pop('treatment_group')
 
     basic_covariates = ['monthly_premium', 'policy_wait']
-    df.\
-        pipe(additional_linear_terms, basic_covariates, t).\
-        pipe(quadratic_interaction_terms)
+    pss = PropensityScoreSpecification(df, t, basic_covariates)
+    print(pss.specification_create().score())
