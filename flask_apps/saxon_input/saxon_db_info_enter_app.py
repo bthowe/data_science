b3954c21389280220e418c20c4e975e52ecd23db
Travis Howe <b.travis.howe@gmail.com>
2018-09-12 09:35:30 -0500

    delete button, unfilled field alert

--- flask_apps/saxon_input/saxon_db_info_enter_app.py
+++ flask_apps/saxon_input/saxon_db_info_enter_app.py
@@ -1,13 +1,8 @@
 import os
 import sys
 import json
-import joblib
-import webbrowser
-import numpy as np
-import pandas as pd
 from pymongo import MongoClient
-from sklearn.externals import joblib
-from flask import Flask, request, render_template, session, flash
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -18,23 +13,28 @@ db = client['math_book_info']
 
 @app.route("/")
 def enter_chapter_details():
-    book = 'Math 5/4'
-    return render_template('enter_chapter_details.html', book=book)
+    return render_template('enter_chapter_details.html')
 
-
-@app.route('/mongo_call', methods=['POST'])
-def mongo_call():
+@app.route('/add_document', methods=['POST'])
+def add_document():
     js = json.loads(request.data.decode('utf-8'))
 
     collection = db[js['book'].replace(' ', '_').replace('/', '_')]
     y = collection.insert_one(js)
     print(y)
 
-    # cursor = collection.find()
-    # for record in cursor:
-    #     print(record)
-
     print('data inserted: {}'.format(js))
+    return ''
+
+@app.route('/remove_document', methods=['POST'])
+def remove_document():
+    js = json.loads(request.data.decode('utf-8'))
+
+    collection = db[js['book'].replace(' ', '_').replace('/', '_')]
+    y = collection.delete_one(js)
+    print(y)
+
+    print('data deleted: {}'.format(js))
     return ''
 
 if __name__ == '__main__':
