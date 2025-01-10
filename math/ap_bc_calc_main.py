84c0ac82b7ad24e266b16c8163bf0703bf9af942
Travis Howe <k_t_howe@traviss-mbp.lan>
2025-01-10 12:17:37 -0600

    integral function

--- math/ap_bc_calc_main.py
+++ math/ap_bc_calc_main.py
@@ -1,4 +1,16 @@
+import sys
+
 import numpy as np
+
+
+
+def integral(f, a, b, precision=10_000):
+    x = np.linspace(a, b, precision)
+
+    width = x[1] - x[0]
+    height = f(x)[1:]
+
+    return np.sum(height * width)
 
 
 def prob_4():
@@ -9,10 +21,20 @@ def prob_4():
 
 
 def prob_6():
-    """limit of infinite sum equivalent to what definite integral"""
-    f = lambda n: np.sum(np.sqrt(3 + ((4 * np.arange(1, n + 1) / n) * (1 / n))))
-    for n in np.arange(1, 1e5):
-        print(f(n))
+    """
+    Limit of infinite sum equivalent to what definite integral: see
+    https://math.stackexchange.com/questions/1356472/turning-infinite-sum-into-integral
+    """
+    f = lambda n: np.sum(np.sqrt(3 + ((4 * np.arange(1, n + 1)) / n)) * (1 / n))
+    print(f(1e6))
+
+    a = integral(lambda x: np.sqrt(3 + 4 * x), 0, 4)
+    b = integral(lambda x: np.sqrt(3 + 4 * x), 3, 7)
+    c = (1 / 4) * integral(lambda x: np.sqrt(x), 3, 7, 1_000_000)
+    d = integral(lambda x: np.sqrt(x), 3, 7)
+
+    print(a, b, c, d)
+
 
 
 def main():
